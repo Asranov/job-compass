1a4377dda4c149dd6076952f7e9643b6203f498b
Asranov <asranov0003@gmail.com>
2024-03-28 16:20:29 +0500

    feat: setup folder structure

---
+++ src/components/CButton/CButton.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CButton.css";
+
+const CButton = () => {
+  return <div>CButton</div>;
+};
+
+export default CButton;
---
+++ src/components/CInput/CInput.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CInput.css";
+
+const CInput = () => {
+  return <div>CInput</div>;
+};
+
+export default CInput;
