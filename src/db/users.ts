597e697e465dbad6c7804cc1719f32e2be445daa
Asranov <asranov0003@gmail.com>
2024-03-30 18:06:19 +0500

    feat: add following card

---
+++ src/db/users.ts
@@ -0,0 +1,40 @@
+import { IUser } from "../models/IUser";
+
+export const users: IUser[] = [
+  {
+    id: 1,
+    name: "Alice",
+    age: 30,
+    profession: "Software Engineer",
+  },
+  {
+    id: 2,
+    name: "Bob",
+    age: 35,
+    profession: "Mobile Developer",
+  },
+  {
+    id: 3,
+    name: "Charlie",
+    age: 28,
+    profession: "Software Architect",
+  },
+  {
+    id: 4,
+    name: "David",
+    age: 32,
+    profession: "Data Scientist",
+  },
+  {
+    id: 5,
+    name: "Eve",
+    age: 27,
+    profession: "Software Engineer",
+  },
+  {
+    id: 6,
+    name: "Frank",
+    age: 33,
+    profession: "DevOps Engineer",
+  },
+];
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,34 @@
 import React from "react";
 import "./Home.css";
 import Header from "../../layouts/Header";
+import { FaRegUserCircle } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa";
+import { users } from "../../db/users";
 
 const Home: React.FC = () => {
   return (
     <div>
       <Header />
       <main className="main__container">
-        <h1>Main</h1>
+        <div className="follow__card">
+          <h3>Following</h3>
+          {users.map((user) => {
+            return (
+              <div key={user.id} className="follow__card-users">
+                <div className="user__details">
+                  <FaRegUserCircle className="user-icon" />
+                  <div>
+                    <p className="user__details-name">{user.name}</p>
+                    <p className="user__details-profession">
+                      {user.profession}
+                    </p>
+                  </div>
+                </div>
+                <FaPlus className="plus-icon" />
+              </div>
+            );
+          })}
+        </div>
       </main>
     </div>
   );
