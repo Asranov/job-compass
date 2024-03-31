a0ea5277650d5a04d01d7677fc6cdd667fa6571c
Asranov <asranov0003@gmail.com>
2024-03-31 11:09:10 +0500

    feat: add vacancies card and render

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,8 +5,8 @@ import { FaRegUserCircle } from "react-icons/fa";
 import { MdMoreVert } from "react-icons/md";
 import { FaPlus } from "react-icons/fa";
 import { users } from "../../db/users";
-import { posts } from "../../db/posts";
 import Footer from "../../layouts/Footer";
+import { vacancies } from "../../db/vacancies";
 
 const Home: React.FC = () => {
   return (
@@ -14,25 +14,44 @@ const Home: React.FC = () => {
       <Header />
 
       <main className="main__container">
-        <div className="posts__wrapper">
-          {posts.map((post) => {
+        <div className="vacancy__card">
+          {vacancies.map((vacancy) => {
             return (
-              <div key={post.id} className="post">
-                <div className="post__owner">
-                  <div className="post__owner-details">
-                    <FaRegUserCircle className="user-icon" />
+              <div key={vacancy.id} className="vacancy__card-wrapper">
+                <div className="vacancy__owner">
+                  <div className="vacancy__owner-details">
+                    <FaRegUserCircle className="icon-user" />
                     <div>
-                      <p className="post__owner-name">{post.owner.name}</p>
-                      <p className="post__owner-profession">
-                        {post.owner.profession}
+                      <p className="vacancy__owner-name">
+                        {vacancy.owner.name}
+                      </p>
+                      <p className="vacancy__owner-profession">
+                        {vacancy.owner.profession}
                       </p>
                     </div>
                   </div>
-                  <MdMoreVert className="more-icon" />
+                  <MdMoreVert className="icon-more" />
                 </div>
-                <div className="post__info">
-                  <h3 className="post__info-title">{post.title}</h3>
-                  <p className="post__info-description">{post.description}</p>
+                <div className="vacancy__info">
+                  <h3 className="vacancy__info-title">{vacancy.title}</h3>
+                  <div className="vacancy__info-details">
+                    <p>{vacancy.description}</p>
+                    <p>- Status: {vacancy.status}</p>
+                    <p>- Location: {vacancy.location}</p>
+                    <p>- Salary: {vacancy.salary}</p>
+                    <p>
+                      - Requirments:
+                      {vacancy.requirements.map((requirement, index) => {
+                        return <p key={index} className="vacancy__info-requirement"> • {requirement}</p>;
+                      })}
+                    </p>
+                    <p>
+                      - Responsibilities:
+                      {vacancy.responsibilities?.map((responsibility, index) => {
+                        return <p key={index} className="vacancy__info-requirement"> • {responsibility}</p>;
+                      })}
+                    </p>
+                  </div>
                 </div>
               </div>
             );
@@ -45,7 +64,7 @@ const Home: React.FC = () => {
             return (
               <div key={user.id} className="follow__card-users">
                 <div className="user__details">
-                  <FaRegUserCircle className="user-icon" />
+                  <FaRegUserCircle className="icon-user" />
                   <div>
                     <p className="user__details-name">{user.name}</p>
                     <p className="user__details-profession">
@@ -53,7 +72,7 @@ const Home: React.FC = () => {
                     </p>
                   </div>
                 </div>
-                <FaPlus className="plus-icon" />
+                <FaPlus className="icon-plus" />
               </div>
             );
           })}
