5ef2acb620021618699012925636405aac31a63b
Asranov <asranov0003@gmail.com>
2024-03-31 11:53:59 +0500

    feat: add not found page

--- src/pages/404/NotFound.tsx
+++ src/pages/404/NotFound.tsx
@@ -1,8 +1,19 @@
 import React from "react";
-import "./NotFound.css"
+import "./NotFound.css";
+import CButton from "../../components/CButton";
 
-const NotFound = () => {
-  return <div>NotFound</div>;
+const NotFound: React.FC = () => {
+  const goBack = () => {
+    window.history.back();
+  };
+
+  return (
+    <div className="not__found">
+      <h2 className="not__found-title">404 - Page Not Found</h2>
+      <p className="not__found-description">Oops! something went wrong</p>
+      <CButton title="Go Back" onClick={goBack} />
+    </div>
+  );
 };
 
 export default NotFound;
